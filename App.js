2a913d40fc03895d1a4fa19edac34ae2e8e34424
Arun Mahajan <153971993+Ghasita@users.noreply.github.com>
2024-05-07 11:53:40 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,67 @@
+
+import './App.css';
+
+
+import Div1 from './Components2/Div1';
+import Div0 from './Components2/Div0';
+import Div2 from './Components2/Div2';
+import Div3 from './Components2/Div3';
+import Div4 from './Components2/Div4';
+import Picture from './components/Picture';
+import Image1 from './components/Image1';
+import Picture2 from './Components2/Picture2';
+import Div5 from './Components2/Div5';
+import Div6 from './Components2/Div6';
+import Div7 from './Components2/Div7';
+import Div8 from './Components2/Div8';
+import Div9 from './Components2/Div9';
+import Picture3 from './Components2/Picture3';
+import Div10 from './Components2/Div10';
+import Div11 from './Components2/Div11';
+import Div12 from './Components2/Div12';
+import Div13 from './Components2/Div13';
+import Div14 from './Components2/Div14';
+import Picture4 from './Components2/Picture4';
+import Div15 from './Components2/Div15';
+import Div16 from './Components2/Div16';
+import Div17 from './Components2/Div17';
+import Div18 from './Components2/Div18';
+function App() {
+
+  return (
+
+    <div className='a'>
+      <div className='a0'><Div0 /></div>
+      <div className='a1'><Div1 /></div>
+      <div className='a2'><Div2 /></div>
+      <div className='a3'><Div3 /></div>
+      <div className='a4'><Div4 /></div>
+      <div className='a5'><Picture/></div>
+     <div className='a6'><Picture2/></div>
+     <div className='a7'><Div5/></div>
+     <div className='a8'><Div6/></div>
+     <div className='a9'><Div7/></div>
+     <div className='a10'><Div8/></div>
+     <div className='a11'><Div9/></div>
+     <div className='a12'><Picture3/></div>
+     <div className='a13'><Div10/></div>
+     <div className='a14'><Div11/></div>
+     <div className='a15'><Div12/></div>
+     <div className='a16'><Div13/></div>
+     <div className='a17'><Div14/></div>
+     <div className='a18'><Picture4/></div>
+     <div className='a19'><Div15/></div>
+     <div className='a20'><Div16/></div>
+     <div className='a21'><Div17/></div>
+     <div className='a22'><Div18/></div>
+     <div className='a23'><Div4 /></div>
+
+    </div>
+
+
+  )
+
+}
+
+
+export default App;
